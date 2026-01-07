41b133af299df7c7c5bc3e95b74be84c6747557b
John Mc Queide <mcqueide@gmail.com>
2026-01-06 22:20:55 -0300

    - Added environment variables.

---
+++ public/env.template.js
@@ -0,0 +1,4 @@
+window.__env = window.__env || {};
+window.__env.KEYCLOAK_URL = '${KEYCLOAK_URL}';
+window.__env.KEYCLOAK_REALM = '${KEYCLOAK_REALM}';
+window.__env.KEYCLOAK_CLIENT_ID = '${KEYCLOAK_CLIENT_ID}';
